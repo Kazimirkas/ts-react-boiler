601e4f63161aa33cd7339315f47e24c94808b97c
Oleksandr Chernenko <ca@nullgr.com>
2018-09-20 13:19:01 +0300

    first component

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import App from './features/App/components/App/App';
 ReactDOM.render(
-<div>
-<h1>Hello, Welcome to the first page</h1>
-</div>,
+  <App />,
   document.getElementById("root")
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 var config = {
   entry: ["./src/index.tsx"],
   output: {
@@ -8,7 +9,6 @@ var config = {
   resolve: {
     extensions: [".ts", ".tsx", ".js"]
   },
-
   module: {
     rules: [
       {
@@ -17,7 +17,21 @@ var config = {
         exclude: /node_modules/
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        title: 'App title',
+        template: path.join('./src/index.ejs'),
+        version: require('./package.json').version,
+        inject: 'body',
+        minify: {
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        hash: true
+      })
+  
+  ]
 };
 
 module.exports = config;
