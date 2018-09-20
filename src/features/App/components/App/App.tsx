4d78d8f0bd485364b65998b2493355b22c1a9f09
Oleksandr Chernenko <ca@nullgr.com>
2018-09-20 15:05:27 +0300

    added prettier and tslint

--- src/features/App/components/App/App.tsx
+++ src/features/App/components/App/App.tsx
@@ -1,12 +1,7 @@
 import * as React from "react";
 
-export default class App extends React.Component <{}> {
-        
-        render() {
-                return (
-                        <div>
-                               I'm a super app component
-                        </div>
-                );
-        }
-};
+export default class App extends React.Component<{}> {
+  render() {
+    return <div>I'm a super app component</div>;
+  }
+}
